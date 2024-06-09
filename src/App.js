ebd8295cf1780cdfbdfc1f468dbff7faaa96f66d
Nickolas Piraino <kwnp17c@gmail.com>
2024-06-09 15:32:22 -0400

    creating the base for the tutorial and checking the ref object in console;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const headerRef = useRef(null);
+  console.log(headerRef)
   return (
     <div className="App">
-      <header className="App-header">
+      <header ref={headerRef} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Scroll down to see sections being revealed by ScrollTrigger.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className='App-main'>
+        <div className='App-section'>
+          <h2>Title</h2>
+          <p>SubTitle</p>
+        </div>
+      </main>
     </div>
   );
 }
