884ade6ce1df8720ef6252b90b0467f016ddfc04
Nickolas Piraino <kwnp17c@gmail.com>
2024-06-09 15:46:15 -0400

    changing the background of the header using gsap and useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react';
 
 function App() {
+  const [background, setBackground] = useState('#262626');
   const headerRef = useRef(null);
+
+  const toggleBackground = () => {
+    const color = background !== '#262626' ? '#5a7d95' : '#1b4943';
+    setBackground(color);
+  }
+
+  useGSAP(() => {
+    gsap.to(headerRef.current, {
+      duration: 1,
+      backgroundColor: background,
+      ease: 'none'
+    });
+  }, [background])
+
   // console.log(headerRef)
   useGSAP(() => {
     gsap.from(headerRef.current, {
@@ -19,6 +34,7 @@ function App() {
     <div className="App">
       <header ref={headerRef} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => toggleBackground()}>Toggle Background</button>
         <p>
           Scroll down to see sections being revealed by ScrollTrigger.
         </p>
